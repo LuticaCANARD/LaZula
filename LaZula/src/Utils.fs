385c40dc069747590809ba881d46ccb27f60b002
Lutica_CANARD <presan100@gmail.com>
2024-10-25 03:47:54 +0900

    초기 커밋.

---
+++ LaZula/src/Utils.fs
@@ -0,0 +1,10 @@
+﻿namespace LaZula
+module Utils =
+    [<Interface>]
+    type ICommand = 
+           abstract member Execute : unit -> unit
+           abstract member CanExecute : unit -> bool
+           abstract member Arguments : Map<string, string>
+    type Config = {
+         cmd_key: string    
+    }
